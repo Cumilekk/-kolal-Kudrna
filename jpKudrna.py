70232e54cc0c8e66e5534e919201e7f2a8ac0e21
Hung Kudrna <159123453+Cumilekk@users.noreply.github.com>
2024-11-06 10:34:13 +0100

    final jpKudrna.py

--- jpKudrna.py
+++ jpKudrna.py
@@ -1,1 +1,23 @@
-arrayA=[4, 8, 9, 7, 7]
+#vytvoření pole a poté vypsaní do console
+arrayA=[4, 8, 9, 7, 3]
+print("pole:", arrayA)
+
+
+#Pomocí metody append() bylo přídano slovo vítr
+arrayA.append("vítr")
+print("pole:", arrayA)
+
+
+#Druhy prvek z pole byl odstraněn pomocí remove()
+arrayA.remove(arrayA[1])
+print("pole:", arrayA)
+
+
+#5 indexová hodnota byla zaměněná za slunce
+arrayA[4]="slunce"
+print("pole:", arrayA)
+
+
+#Byly přídany dvě stringové hodnoty pomocí extend()
+arrayA.extend(["děšť, sníh"])
+print("pole:", arrayA)
